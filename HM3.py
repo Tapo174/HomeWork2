e8b5694f624656f3d8b055804d0d5039975649c8
Vladimir <Vladimir08122000@gmail.com>
2023-01-09 21:01:22 +0500

    finish

--- HM3.py
+++
@@ -1,9 +0,0 @@
-# Задайте список из n чисел последовательности $(1+\frac 1 n)^n$ и выведите на экран их сумму.
-
-
-N = int(input('Введите число: ')) 
-
-result = [round((1 + 1 / i)**i, 2) for i in range (1, N + 1)]   
-        
-print(result)
-print(round(sum(result)))
