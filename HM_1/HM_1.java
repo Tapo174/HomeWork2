44e5b4b18392d04cfd669e00ce8a517a6a7bf828
Vladimir <Vladimir08122000@gmail.com>
2023-02-24 01:08:51 +0500

    Finishedd

---
+++ HM_1/HM_1.java
@@ -0,0 +1,12 @@
+package HM_1;
+import java.util.Scanner;
+public class HM_1 {
+    public static void main(String[] args) {
+        System.out.print("Введите последовательность треугольных чисел: ");
+        Scanner in = new Scanner(System.in);
+        int user_input = in.nextInt();
+        int res = (user_input*(user_input+1))/2;
+        System.out.println(res);
+    }
+    
+}
